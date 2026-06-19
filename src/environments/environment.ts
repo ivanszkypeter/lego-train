15f0dc0356e9fa84acc5c3d08ae242655163093f
Péter Ivánszky <ivanszkypeter@gmail.com>
2026-06-19 16:29:08 +0200

    Add live update environment config

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  liveUpdate: {
+    enabled: true,
+    channel: 'stable',
+    manifestUrl: 'https://ivanszkypeter.github.io/lego-train/live-update/manifest.json',
+    checkOnStartup: true,
+    reloadWhenTrainIsIdle: true
+  }
+};
